6320b00d9fc7c69241d83c9b0c93f55a542826c6
cyborg <3430575+nutcase84@users.noreply.github.com>
2021-01-31 19:21:06 -0500

    cleanup

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 filename="pipelined-mandelbrot.v"
 
-rm testbench.vvp
-rm testbench.vcd
 iverilog -s testbench -o testbench.vvp $filename
 vvp testbench.vvp
 gtkwave testbench.vcd
+rm testbench.vvp
+rm testbench.vcd
